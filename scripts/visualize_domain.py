65e641da19c0cb98e762f583da38997eeadf0acf
mw483 <159742840+mw483@users.noreply.github.com>
2026-07-03 17:09:50 +0900

    new map

--- scripts/visualize_domain.py
+++ scripts/visualize_domain.py
@@ -10,16 +10,16 @@ from plotting_core.map_plots import plot_birdseye_domain
 
 def main():
     # 1. Paths
-    flat_map_path = r"Z:\map\map_01_flat_plane.dat"
-    cube_map_path = r"Z:\map\map_02_full_roughness.dat"
+    flat_map_path = r"C:\Users\Mikael Wijaya\Documents\GitHub\lbm-cuda-project\map\map_flat_16m_shortroughness.dat"
+    # cube_map_path = r"Z:\map\map_02_full_roughness.dat"
     
-    flat_pos_path = r"Z:\particle_position\pos_flat_3072.txt"
-    cube_pos_path = r"Z:\particle_position\pos_cube_3072.txt"
+    flat_pos_path = r"C:\Users\Mikael Wijaya\Documents\GitHub\lbm-cuda-project\particle_position\pos_flat_3072_shortroughness.txt"
+    # cube_pos_path = r"Z:\particle_position\pos_cube_3072.txt"
     
     output_dir = r"../figures/map_visualizations"
     
     # Sensor Coordinates & Grid Resolution
-    sensor_x = 600.0
+    sensor_x = 728.0
     sensor_y = 128.0
     dx = 2.0
     dy = 2.0
@@ -36,25 +36,25 @@ def main():
         source_map=flat_sources, 
         sensor_pos=(sensor_x, sensor_y), 
         title="Bird's-Eye View: Flat Domain Source Distribution", 
-        save_path=os.path.join(output_dir, "domain_flat_plane.png"),
+        save_path=os.path.join(output_dir, "domain_flat_plane_shortroughness.png"),
         dx=dx, dy=dy
     )
 
-    # ==========================================
-    # DOMAIN 2: CUBE ROUGHNESS
-    # ==========================================
-    print("Visualizing Cube Domain...")
-    cube_matrix, _, _ = load_lbm_map(cube_map_path)
-    cube_sources = load_source_positions(cube_pos_path)
+    # # ==========================================
+    # # DOMAIN 2: CUBE ROUGHNESS
+    # # ==========================================
+    # print("Visualizing Cube Domain...")
+    # cube_matrix, _, _ = load_lbm_map(cube_map_path)
+    # cube_sources = load_source_positions(cube_pos_path)
     
-    plot_birdseye_domain(
-        elevation_matrix=cube_matrix, 
-        source_map=cube_sources, 
-        sensor_pos=(sensor_x, sensor_y), 
-        title="Bird's-Eye View: Cube Canopy Source Distribution", 
-        save_path=os.path.join(output_dir, "domain_cubes.png"),
-        dx=dx, dy=dy
-    )
+    # plot_birdseye_domain(
+    #     elevation_matrix=cube_matrix, 
+    #     source_map=cube_sources, 
+    #     sensor_pos=(sensor_x, sensor_y), 
+    #     title="Bird's-Eye View: Cube Canopy Source Distribution", 
+    #     save_path=os.path.join(output_dir, "domain_cubes.png"),
+    #     dx=dx, dy=dy
+    # )
     
     print(f"Visualizations generated successfully in {output_dir}")
 
